a8085cb64bb731167493a4f76196b8a41435e9cf
Buttonupd <Buttonupdan1199@gmail.com>
2021-08-16 15:22:38 +0300

    Adding userprofiles

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,126 @@
-import "./App.css";
-import React, { useState } from "react";
 import axios from "axios";
+import "./App.css";
+import stubs from "./defaultStubs";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 function App() {
   const [code, setCode] = useState("");
-  const [output, setOutPut] = useState("");
+  const [output, setOutput] = useState("");
   const [language, setLanguage] = useState("cpp");
-  const [status, setStatus] = useState("");
-  const [jobId, setJobId] = useState("");
+  const [jobId, setJobId] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [jobDetails, setJobDetails] = useState(null);
+
+  useEffect(() => {
+    setCode(stubs[language]);
+  }, [language]);
+
+  useEffect(() => {
+    const defaultLang = localStorage.getItem("default-language") || "cpp";
+    setLanguage(defaultLang);
+  }, []);
+
+  let pollInterval;
 
   const handleSubmit = async () => {
     const payload = {
-      language: language,
+      language,
       code,
     };
     try {
-      setJobId("");
-      setStatus("");
-      setOutPut("");
-      const { data} = await axios.post("http://127.0.0.1:5000/run", payload);
-      console.log(data);
-      setJobId(data.jobId);
-      let intervalId;
-      
-      intervalId = setInterval( async() =>{
-        const {data:dataRes} = await axios.get("http://127.0.0.1:5000/status",
-        {params: {id: data.jobId}});
-        const {success, job, error} = dataRes;
-      
-        if(success){
-          const {status: jobStatus, output:jobOutput} = job;
-          setStatus(jobStatus);
-          if (jobStatus === "pending") return;
-          setOutPut(jobOutput);
-          clearInterval(intervalId);
-        }
-        else{
-          setStatus("Error Please Retry");
-          console.log(error);
-          clearInterval(intervalId);
-          setOutPut(error)
-        }
-        console.log(dataRes);
-      }, 1000)  ;
-      
+      setOutput("");
+      setStatus(null);
+      setJobId(null);
+      setJobDetails(null);
+      const { data } = await axios.post("http://localhost:5000/run", payload);
+      if (data.jobId) {
+        setJobId(data.jobId);
+        setStatus("Submitted.");
+
+        // poll here
+        pollInterval = setInterval(async () => {
+          const { data: statusRes } = await axios.get(
+            `http://localhost:5000/status`,
+            {
+              params: {
+                id: data.jobId,
+              },
+            }
+          );
+          const { success, job, error } = statusRes;
+          console.log(statusRes);
+          if (success) {
+            const { status: jobStatus, output: jobOutput } = job;
+            setStatus(jobStatus);
+            setJobDetails(job);
+            if (jobStatus === "pending") return;
+            setOutput(jobOutput);
+            clearInterval(pollInterval);
+          } else {
+            console.error(error);
+            setOutput(error);
+            setStatus("Bad request");
+            clearInterval(pollInterval);
+          }
+        }, 1000);
+      } else {
+        setOutput("Retry again.");
+      }
     } catch ({ response }) {
       if (response) {
         const errMsg = response.data.err.stderr;
-        setOutPut(errMsg)
-
+        setOutput(errMsg);
       } else {
-        setOutPut("Error connecting to server");
+        setOutput("Please retry submitting.");
       }
     }
   };
 
+  const setDefaultLanguage = () => {
+    localStorage.setItem("default-language", language);
+    console.log(`${language} set as default!`);
+  };
+
+  const renderTimeDetails = () => {
+    if (!jobDetails) {
+      return "";
+    }
+    let { submittedAt, startedAt, completedAt } = jobDetails;
+    let result = "";
+    submittedAt = moment(submittedAt).toString();
+    result += `Job Submitted At: ${submittedAt}  `;
+    if (!startedAt || !completedAt) return result;
+    const start = moment(startedAt);
+    const end = moment(completedAt);
+    const diff = end.diff(start, "seconds", true);
+    result += `Execution Time: ${diff}s`;
+    return result;
+  };
+
   return (
     <div className="App">
-      <h1>Online Compiler</h1>
+      <h1>Online Code Compiler</h1>
       <div>
         <label>Language: </label>
         <select
           value={language}
           onChange={(e) => {
-            setLanguage(e.target.value);
-            console.log(e.target.value);
+            const shouldSwitch = window.confirm(
+              "Are you sure you want to change language? WARNING: Your current code will be lost."
+            );
+            if (shouldSwitch) {
+              setLanguage(e.target.value);
+            }
           }}
         >
           <option value="cpp">C++</option>
           <option value="py">Python</option>
-          <option value="js">Javascript</option>
-          <option value="war">Java</option>
-          <option value="c">C</option>
         </select>
+      </div>
+      <br />
+      <div id="default">
+        <button class="color" onClick={setDefaultLanguage}>Set Default</button>
       </div>
       <br />
       <textarea
@@ -85,9 +133,10 @@ function App() {
       ></textarea>
       <br />
       <button onClick={handleSubmit}>Submit</button>
-      <p>{output}</p>
       <p>{status}</p>
-      <p>{jobId && `jobId:${jobId}`}</p>
+      <p>{jobId ? `Job ID: ${jobId}` : ""}</p>
+      <p>{renderTimeDetails()}</p>
+      <p>{output}</p>
     </div>
   );
 }
